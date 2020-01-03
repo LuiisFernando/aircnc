3d75e1ec60bac89665e9382f2dd54eca794c9e50
Luis Fernando <lsh.fernandors@gmail.com>
2020-01-02 22:20:29 -0300

    creating route to static file as image and added virutal propertie on Spot, creating project frontend and implementing Login and Dashboard page

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -12,9 +14,11 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
